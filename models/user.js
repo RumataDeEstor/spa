896d3c48f6e9fc075e20129031aff847eeba3430
Rumata <Rumatadeestor@gmail.com>
2016-08-04 14:26:24 +0300

    passport.js auth added

--- models/user.js
+++ models/user.js
@@ -22,12 +22,16 @@ var userSchema = new Schema({
   projects: [projectSchema]
 });
 
+userSchema.methods.validPassword = function(pass){
+  return this.password === pass;
+}
+
 userSchema.path('login').validate(function (v) {
-    return v.length > 2 && v.length < 25;
+  return v.length > 2 && v.length < 25;
 });
 
 userSchema.path('password').validate(function (v) {
-    return v.length > 5 && v.length < 30;
+  return v.length > 5 && v.length < 30;
 });
 
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,26 +1,70 @@
-var express = require('express');
-var debug = require('debug')('server:');
-var bodyParser = require('body-parser');
-var path = require('path'); 
-var User = require('./models/user');
+let express = require('express');
+let debug = require('debug')('server:');
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+let session = require('express-session');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+let flash = require('express-flash');
+let path = require('path'); 
+let User = require('./models/user');
 
-var app = express();
-var port = process.env.PORT || 8080;
+let app = express();
+let port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false}
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.engine('ejs', require('ejs-mate'));
+passport.use(new LocalStrategy({
+    usernameField: 'login',
+    passwordField: 'password'
+  },
+  function(login, password, done) {
+    User.findOne({ login: login }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        debug('Incorrect username');
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        debug('invalid password');
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      debug('valid');
+      return done(null, user);
+    });
+  }
+));
 
-app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'ejs');
+passport.serializeUser(function(user, done) {
+  debug('ser');
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  debug('deser');
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 app.use((req,res,next) => {
   debug(`req to ${req.url}. AJAX: ${req.xhr}`);
   next();
 });
 
-app.get('/allusers', (req,res,next) => {
+app.get('/allusers', (req,res,next) => {  // only for debugging!
+  // but if DB doesn't work?
   User.find({}, (err, result) => {
     debug(err);
     res.send(result);
@@ -29,15 +73,15 @@ app.get('/allusers', (req,res,next) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post('/signup',(req,res,next) => {
+app.post('/signup', (req,res,next) => {
   let user = new User({
-      login: req.body.login,
-      password: req.body.password
+    login: req.body.login,
+    password: req.body.password
   });
  
   user.save((err) => {
-    debug(err);
     if (err) {
+    debug(err);
       if (err.code && err.code == 11000) { // MongoDB:if login is not unique
         res.statusCode = 403;
         res.send({ error: 'duplicate' });        
@@ -48,52 +92,64 @@ app.post('/signup',(req,res,next) => {
         res.statusCode = 500;
         res.send({ error: 'Server error' });
       }
-
     } else {
       res.status(200).send(`Welcome aboard, ${user.login}!`);
     }
   });
-
 });
-
    
-app.post('/login', (req,res,next) => {
-// req.login
-// req.password
-// if login exist check password
-//   if password matches can go to userdata/:login
-//   if wrong password response wrong
-// if login doesnt exist No such user
-User.findOne({login: req.body.login}, (err, result) => {
-    if (err) { 
-      debug(err);
-      res.status(500).send({error: 'Internal Server Error'});
-    }   
-    else if (result) { 
-      if (result.password === req.body.password) {
-        res.status(200).send('Ok, I recognize you.');
-      } else {
-        res.status(403).send({error: 'Wrong password'});
-      }
-    } else res.status(404).send({error: "We don't have such user"});
-  });
+// app.post('/login', (req,res,next) => {
+// User.findOne({login: req.body.login}, (err, result) => {
+//     if (err) { 
+//       debug(err);
+//       res.status(500).send({error: 'Internal Server Error'});
+//     }   
+//     else if (result) { 
+//       if (result.password === req.body.password) {
+//         res.status(200).send('Ok, I recognize you.');
+//       } else {
+//         res.status(403).send({error: 'Wrong password'});
+//       }
+//     } else res.status(404).send({error: "We don't have such user"});
+//   });
+// });
+
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return res.status(err.statusCode).send({error: err.message}); };
+    if (!user) { return res.status(401).send({error: info.message}); }
+    req.logIn(user, function(err) {
+      if (err) { return res.status(err.statusCode).send({error: err.message}); }
+      return res.redirect('/userdata/' + user.login);
+    });
+  })(req, res, next);
 });
 
+let isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
 
-app.get('/userdata/:login', (req, res, next) => {
+app.get('/userdata/:login',  isAuthenticated, (req, res, next) => {
+  if (req.params.login !== req.user.login) {  // can't get other people's page
+    return res.status(403).send({error: 'Forbidden'});
+  }
   User.findOne({login: req.params.login}, (err, result) => {
     if (err) { 
       debug(err);
       res.status(500).send('Internal Server Error');
     } else {
+      debug('success get proj');
       res.send(result.projects);
     }
   });
 });
-app.post('/userdata/:login',(req,res,next) => {
-  // add user projects in DB:
-  //find in DB login 
-  // add req.body.project in Rumata 
+
+app.post('/userdata/:login', isAuthenticated, (req,res,next) => {
+  if (req.params.login !== req.user.login) {  
+    return res.status(403).send({error: 'Forbidden'});
+  }
   User.findOne({login: req.params.login}, (err, result) => {
     if (err) { 
       debug(err);
@@ -119,7 +175,10 @@ app.post('/userdata/:login',(req,res,next) => {
   })  
 });
 
-app.delete('/userdata/:login/:projectID', (req, res, next) => {
+app.delete('/userdata/:login/:projectID', isAuthenticated, (req, res, next) => {
+  if (req.params.login !== req.user.login) {  
+    return res.status(403).send({error: 'Forbidden'});
+  }
   User.findOne({login: req.params.login}, (err, result) => {
     if (err) {
       debug(err);
@@ -138,10 +197,6 @@ app.delete('/userdata/:login/:projectID', (req, res, next) => {
     })
   })
 })
-
-// app.get('/userdata/:login',(req,res,next) => {
-//   res.end(`You are ${req.params.login}`);
-// });
 
 app.use((req, res, next) => {
 	let err = new Error('Not Found');
