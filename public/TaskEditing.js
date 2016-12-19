3623f3df8ec8762a8d4df7012e8afe3d1ceb48ca
Rumata <Rumatadeestor@gmail.com>
2016-12-19 04:45:37 +0300

    form validation for editing task

--- public/TaskEditing.js
+++ public/TaskEditing.js
@@ -15,15 +15,46 @@ export default class TaskEditing extends React.Component {
     this.onFinishEdit = this.onFinishEdit.bind(this);
     this.onDelete = this.onDelete.bind(this);
     this.checkRepeated = this.checkRepeated.bind(this);
+    this.checkForm = this.checkForm.bind(this);
+    this.checkName = this.checkName.bind(this);
+    this.trimName = this.trimName.bind(this);
   }
 
   componentDidMount() {
     // setTimeout( () =>{
     //   editForm.style.height = "76px";
     // }, 1);    
+    this.refs.editTaskForm.addEventListener('submit', this.checkForm, false);
   }
 
-  onFinishEdit() {
+  checkForm (e) {
+    e.preventDefault();
+    this.trimName();
+    this.normalizePoints();
+    if ( this.checkName() ) this.saveChanges();
+  }
+
+  trimName () {
+    this.refs.editTaskName.value = this.refs.editTaskName.value.trim();
+    let validName = (this.refs.editTaskName.value.length > 17) ? 
+      this.refs.editTaskName.value.slice(0, 17) : this.refs.editTaskName.value;
+    this.refs.editTaskName.value = validName;
+  }
+
+  normalizePoints () {
+    let points = this.refs.editPoints.value;
+    let newPoints = this.refs.editPoints.defaultValue;
+    this.refs.editPoints.value = ( (points > 0) && 
+      (points < 501) ) ? points : newPoints;  
+  }
+
+  checkName () {
+    let name = this.refs.editTaskName.value;
+    return (/^(\w|\s|[А-Яа-яёЁ])*$/.test(name));   
+  }
+
+  onFinishEdit(e) {
+    if (e) e.preventDefault();
     // let promise = new Promise (resolve => {
     //   editForm.style.height = "0";
     //   setTimeout( () =>{
@@ -39,7 +70,10 @@ export default class TaskEditing extends React.Component {
     this.refs.cRep.className = (this.refs.cRep.className == "checked") ? "unchecked" : "checked";
   }
 
-  onDelete(){   // mustn't be repeated with TasksItem
+  onDelete(e){   // mustn't be repeated with TasksItem
+
+    if (e) preventDefault();
+
     let reqParams = {
       method: 'DELETE',
       credentials: 'include'
@@ -65,7 +99,7 @@ export default class TaskEditing extends React.Component {
 
   saveChanges () {
     let repeated = this.refs.cRep.className == "checked";
-    console.log(repeated);
+   
     let bodyJSON = JSON.stringify({
       name: this.refs.editTaskName.value,
       points: this.refs.editPoints.value,
@@ -107,30 +141,33 @@ export default class TaskEditing extends React.Component {
   render () {
       let isRepeated = this.props.target.props.repeated ? "checked" : "unchecked";
       return<div className = "editForm">
-              <input type = "text" defaultValue = {this.props.target.props.name} 
-                className = "editTaskName"
-                ref = "editTaskName"
-              />
-              <div className = "editOpt">
-                <input type = "number" className = "editPoints" 
-                  ref = "editPoints"
-                  defaultValue = {this.props.target.props.points} 
-                  min = "0" max = "500"
+              <form ref = "editTaskForm" className = "editTask">
+                <input type = "text" defaultValue = {this.props.target.props.name} 
+                  className = "editTaskName"
+                  ref = "editTaskName"
+                  maxLength = "17"
                 />
-                <div id = "editCheckBoxRepeated" 
-                  className = {isRepeated}
-                  ref = "cRep"
-                  onClick = {this.checkRepeated}>
-                  <i className="fa fa-repeat" aria-hidden="true"></i>
+                <div className = "editOpt">
+                  <input type = "number" className = "editPoints" 
+                    ref = "editPoints"
+                    defaultValue = {this.props.target.props.points} 
+                    min = "1" max = "500"
+                  />
+                  <div id = "editCheckBoxRepeated" 
+                    className = {isRepeated}
+                    ref = "cRep"
+                    onClick = {this.checkRepeated}>
+                    <i className="fa fa-repeat" aria-hidden="true"></i>
+                  </div>
+                  <div className = "editButtons">      
+                    <input type = "submit" className = "editSave" value = "Save"/>    
+                    <button className = "editFinish" onClick = {this.onFinishEdit}>Cancel</button>
+                    <button className = "editDelete" onClick = {this.onDelete}>
+                      <i className="fa fa-trash"></i>
+                    </button>                  
+                  </div>  
                 </div>
-                <div className = "editButtons">           
-                  <button className = "editSave" onClick = {this.saveChanges}>Save</button>
-                  <button className = "editFinish" onClick = {this.onFinishEdit}>Cancel</button>
-                  <button className = "editDelete" onClick = {this.onDelete}>
-                    <i className="fa fa-trash"></i>
-                  </button>                  
-                </div>  
-              </div>
+              </form>
             </div>
   }
 }
